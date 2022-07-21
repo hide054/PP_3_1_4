38fce043b35800f732d38885ec5f10cd5c6af51f
mariya-pshenichnova <m.pshe@yandex.ru>
2022-07-21 13:47:29 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package web.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    User getUserById(long id);
+
+}
